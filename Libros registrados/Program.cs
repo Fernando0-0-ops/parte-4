660809842ca1cb5aadb0243ee964b53725ff5c27
Mini Yoy <miniyoy44@gmail.com>
2026-03-17 18:53:16 -0600

    Agregar archivos de proyecto.

---
+++ Libros registrados/Program.cs
@@ -0,0 +1,15 @@
+﻿//Andy Yoel Cardona Paz
+
+// Mostrar libros registrados
+string titulo = "Clean code";
+string autor = "Robert Martin";
+string categoria = "Tecnologia";
+
+// Mostrar la información del libro registrado
+Console.WriteLine("Lista de libros registrados");
+Console.WriteLine();
+
+// Mostrar la información del libro registrado
+Console.WriteLine("Titulo: " + titulo);
+Console.WriteLine("Autor: " + autor);
+Console.WriteLine("Categoria: " + categoria);
