5c446172acde50c36143e5975b13140a6f52dfb7
Fernandiho O__O <ari.pretzantzin@gmail.com>
2026-03-17 18:33:57 -0600

    Agregar archivos de proyecto.

---
+++ parte-4/Program.cs
@@ -0,0 +1,31 @@
+﻿///(Fernando Pretzantzin)
+
+Console.WriteLine("Registrar prestamo");
+
+// Estas variables deberían venir del sistema principal4
+string titulo1 = "Clean Code";
+bool prestado1 = false;
+
+Console.WriteLine("Ingrese nombre de usuario");
+string usuario = Console.ReadLine();
+
+Console.WriteLine("Ingrese Titulo del libro");
+string libro = Console.ReadLine();
+
+if (libro == titulo1)
+{
+    if (prestado1 == true)
+    {
+        Console.WriteLine("Advertencia: El libro ya está prestado");
+    }
+    else
+    {
+        prestado1 = true;
+        Console.WriteLine("Usuario: " + usuario);
+        Console.WriteLine("Libro prestado: " + libro);
+    }
+}
+else
+{
+    Console.WriteLine("El libro no existe");
+}//
